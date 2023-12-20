c906ad39d39c8bc8066b63bd5e858cfa326b6e04
Adi Rao <adityakrao.projects@gmail.com>
2023-12-20 04:32:57 -0500

    first commit

---
+++ latex/code_examples/chapter_5/list_def.py
@@ -0,0 +1,18 @@
+# Creating a list in Python
+
+# The following is an empty list
+lst_def = []
+
+# or this does the same thing (but this isn't the preferred way)
+lst_def = list() 
+
+# The following is a list of integers
+lst_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+
+
+# The following is a list of characters
+lst_2 = ['A', 'B', 'C', 'D']
+
+
+# The following is a list of multiple data types
+lst_3 = [1, 2.3, 'A', 'Word', True]
---
+++ latex/code_examples/chapter_5/tuple_def.py
@@ -0,0 +1,18 @@
+# Creating a tuple in Python
+
+# The following is an empty tuple
+tup_def = ()
+
+# or this does the same thing (but this isn't the preferred way)
+tup_def = tuple() 
+
+# The following is a list of integers
+tup_1 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) 
+
+
+# The following is a list of characters
+tup_2 = ('A', 'B', 'C', 'D')
+
+
+# The following is a list of multiple data types
+tup_3 = (1, 2.3, 'A', 'Word', True)
